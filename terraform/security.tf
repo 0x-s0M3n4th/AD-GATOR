05dbc8598bde350c8c3083ad53674c4c8103d128
0x-s0M3n4th <sebaitsom6297@gmail.com>
2026-04-26 19:12:15 +0530

    [TERRAFORM] : Added NSGs(Firewall rules) for SSH and RDP for the 4 machines to be accessed via internet.

---
+++ terraform/security.tf
@@ -0,0 +1,38 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "ad-gator-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Allow RDP (for Windows machines)
+
+resource "azurerm_network_security_rule" "rdp" {
+  name                        = "Allow-RDP"
+  priority                    = 1000
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+# source_address_prefix means anyone can connect to this machine using RDP from the internet.
+
+# Allow SSH (for Kali/Linux)
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.my_ip
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
