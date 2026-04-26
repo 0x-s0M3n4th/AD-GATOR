9083d9a187aa3bbb7dd4a74f74722118a4fc782a
0x-s0M3n4th <sebaitsom6297@gmail.com>
2026-04-26 19:11:04 +0530

    [TERRAFORM] : Added configuration for virtual network and it's 4 subnets for 4 machines

---
+++ terraform/network.tf
@@ -0,0 +1,47 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "ad-gator-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Subnets
+# Domain Controller
+
+resource "azurerm_subnet" "domain" {
+  name                 = "domain-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Windows workstations
+
+resource "azurerm_subnet" "workstation" {
+  name                 = "workstation-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Kali-Linux
+
+resource "azurerm_subnet" "attacker" {
+  name                 = "attacker-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Wazuh
+
+resource "azurerm_subnet" "monitoring" {
+  name                 = "monitoring-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
